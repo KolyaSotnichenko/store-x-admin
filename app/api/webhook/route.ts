20669b4dbe685b2d1080cae1b28f0f98da6b8938
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2024-02-19 11:52:59 +0200

    fix signature

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,14 +1,14 @@
 import Stripe from "stripe";
 import { headers } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   const body = await req.text();
-  // const signature = headers().get("Stripe-Signature") as string;
-  const signature = req.headers.get("stripe-signature");
+  const signature = headers().get("Stripe-Signature") as string;
+  // const signature = req.headers.get("stripe-signature");
 
   let event: Stripe.Event;
 
